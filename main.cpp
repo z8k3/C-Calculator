9aeaceb864f257b9bcd74a763970b1d0056cf5ef
z8k3 <144612805+z8k3@users.noreply.github.com>
2023-11-03 20:54:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+  cout << "Multiply, divide, add, or subtract?" << endl;
+  string choice;
+  cin >> choice;
+  double x;
+  double y;
+  cin >> x;
+  cin >> y;
+  if (choice == "multiply" || choice == "Multiply") {
+    cout << x * y << endl;
+  } else if (choice == "add" || choice == "Add") {
+    cout << x + y << endl;
+  } else if (choice == "divide" || choice == "Divide") {
+    cout << x / y << endl;
+  } else if (choice == "subtract" || choice == "Subtract") {
+    cout << x - y << endl;
+  }
+  return 0;
+}
